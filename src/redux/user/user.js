8b73bef59f621c31cebd635e6f4b54d01fa2604d
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-22 20:21:07 +0500

    Delete unnecessary files

--- src/redux/user/user.js
+++
@@ -1,6 +0,0 @@
-export default function reducer(state = { user: { id: 1, name: 'Hayya' }, errors: [], notices: [] }, action = {}) {
-  switch (action.type) {
-    default:
-      return state;
-  }
-}
