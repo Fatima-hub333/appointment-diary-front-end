bca4cb51f2d57fac67361ffe97d9044fb53a5a7d
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-22 22:43:03 +0500

    Fix linter errors

--- src/App.js
+++ src/App.js
@@ -6,24 +6,24 @@ import Reserve from './components/Reserve';
 import MyReservations from './components/MyReservations';
 import DeleteVehicle from './components/DeleteVehicle';
 import Vehicles from './components/Vehicles';
-import VehicleDetails from './components/VehicleDetails'
+import VehicleDetails from './components/VehicleDetails';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <NavigationPanel />
-     <Routes>
-        <Route path="/" element={(<Login/>)}/>
-        <Route path="/sign_up"  element={<SignUp clickHandler={clickHandler} />}/>
-        <Route path="/main" element={(<Vehicles/>)}/>
-        <Route path="/vehicle" element={(<DeleteVehicle/>)}/>
-        <Route path="/vehicles/:vehicleId/details" element={(<VehicleDetails/>)}/>
-        <Route path="/vehicles/:vehicleId/reservation" element={(<Reserve/>)}/>
-        <Route path="/reservation" element={(<Reserve/>)}/>
-        <Route path="/user/reservations" element={(<MyReservations/>)}/>
+      <NavigationPanel />
+      <Routes>
+        <Route path="/" element={(<Login />)} />
+        <Route path="/sign_up" element={<SignUp />} />
+        <Route path="/main" element={(<Vehicles />)} />
+        <Route path="/vehicle" element={(<DeleteVehicle />)} />
+        <Route path="/vehicles/:vehicleId/details" element={(<VehicleDetails />)} />
+        <Route path="/vehicles/:vehicleId/reservation" element={(<Reserve />)} />
+        <Route path="/reservation" element={(<Reserve />)} />
+        <Route path="/user/reservations" element={(<MyReservations />)} />
       </Routes>
-    </div>  
+    </div>
   );
 }
 
