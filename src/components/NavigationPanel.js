922de88833c5b2b9dd61df870342badfe548944b
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2022-09-23 22:34:21 +0430

    Add Navigation links

--- src/components/NavigationPanel.js
+++ src/components/NavigationPanel.js
@@ -1,6 +1,42 @@
+import { Link } from 'react-router-dom';
+
 function NavigationPanel() {
   return (
-    <div className="NavigationPanel" />
+    <div className="NavigationPanel">
+      <div className="App">
+        <nav>
+            <div>
+              <ul>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/main">
+                    Vehicles
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/reservation">
+                    Reserve
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/user/reservations">
+                    My reservations
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/vehicle">
+                    Add a vehicle
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/vehicle">
+                    Delete vehicle
+                  </Link>
+                </li>
+              </ul>
+          </div>
+        </nav>
+      </div>
+    </div>
   );
 }
 
