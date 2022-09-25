356332b8eb03f5c40eccf2b5f4eecaa88777afdb
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-25 12:21:55 +0500

    Add vehicle page

--- src/components/AddVehicle.js
+++ src/components/AddVehicle.js
@@ -1,7 +1,72 @@
-function Addvehicle() {
+import { useState } from 'react';
+import {
+  Container, Row, Col, Alert, Form, Button,
+} from 'react-bootstrap';
+
+function AddVehicle() {
+  const [errors, setErrors] = useState([]);
+  const [{
+    id, price, name, image,
+  }, setVehicle] = useState({});
+
+  const onSelectFile = (e) => {
+    if (!e.target.files || e.target.files.length === 0) {
+      setVehicle({
+        id, name, price, image: undefined,
+      });
+      return;
+    }
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      setVehicle({
+        id, name, price, image: reader.result,
+      });
+    };
+    reader.onerror = () => {
+      setErrors([`Error occurred reading file: ${file.name}`]);
+    };
+  };
+  const handleChange = (e) => {
+    setVehicle({
+      id, name, price, image, [e.target.name]: e.target.value,
+    });
+  };
   return (
-    <div className="AddVehicle" />
+    <Form>
+      <Container className="AddVehicle">
+        <Row>
+          <Col>
+            { errors.map((error) => (
+              <Alert key={error} variant="danger">
+                {error}
+              </Alert>
+            )) }
+          </Col>
+        </Row>
+        <Row>
+          <Col lg={8}>
+            <div className="image-panel">
+              <input type="file" onChange={onSelectFile} />
+              {image ? <img src={image} alt={name} /> : 'Plese select an image' }
+            </div>
+          </Col>
+          <Col lg={4}>
+            <Row>
+              <Form.Control type="text" placeholder="Name" name="name" value={name} onChange={handleChange} />
+            </Row>
+            <Row>
+              <Form.Control type="number" placeholder="Price" name="price" value={price} onChange={handleChange} />
+            </Row>
+            <Row>
+              <Button variant="primary" type="submit">Create</Button>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    </Form>
   );
 }
 
-export default Addvehicle;
+export default AddVehicle;
