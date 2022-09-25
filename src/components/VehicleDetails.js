5cf7c0e2ac4c5d72d7a07f0298f78b939801d43a
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-25 13:24:02 +0500

    Fix linter errors

--- src/components/VehicleDetails.js
+++ src/components/VehicleDetails.js
@@ -1,10 +1,34 @@
-import { useParams } from 'react-router';
+import React from 'react';
 
 function VehicleDetails() {
-  const { vehicleId } = useParams();
+  const vehicles = [
+    {
+      id: 1,
+      name: 'vehicle 1',
+      description: 'This is a description for vehicle 1',
+      image: 'https://images.unsplash.com/photo-1610553556003-9b2ae8ef1b8e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=327&q=80',
+      price: '$100',
+    },
+  ];
+  const vehicle = vehicles[0];
   return (
-    <div className="VehicleDetails">
-      {vehicleId}
+    <div>
+      <div>
+        <h1>Vehicle Details</h1>
+      </div>
+      <div>
+        <div className="details-card">
+          <img className="card-img-det" src={vehicle.image} alt="Card cap" />
+          <div className="card-body d-flex justify-content-between img-text">
+            <h5 className="card-title">{vehicle.name}</h5>
+            <p className="card-text">{vehicle.price}</p>
+          </div>
+        </div>
+        <div>
+          <p>About this vehicle</p>
+          <p>{vehicle.description}</p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Vehicles.js
+++ src/components/Vehicles.js
@@ -1,6 +1,59 @@
+import { Link } from 'react-router-dom';
+
 function Vehicles() {
+  const vehicles = [
+    {
+      id: 1,
+      name: 'vehicle 1',
+      description: 'This is a description for vehicle 1',
+      image: 'https://images.unsplash.com/photo-1558981403-c5f9899a28bc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YmlrZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+      price: '$100',
+    },
+    {
+      id: 2,
+      name: 'vehicle 2',
+      description: 'This is a description for vehicle 2',
+      image: 'https://images.unsplash.com/photo-1617109887854-f661d37fca2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YmlrZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+      price: '$200',
+    },
+    {
+      id: 3,
+      name: 'vehicle 3',
+      description: 'This is a description for vehicle 3',
+      image: 'https://images.unsplash.com/photo-1626840362735-afb64615318d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8YmlrZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+      price: '$300',
+    },
+    {
+      id: 4,
+      name: 'vehicle 4',
+      description: 'This is a description for vehicle 4',
+      image: 'https://images.unsplash.com/photo-1616839273372-a16bca55cc47?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGJpa2VzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+      price: '$400',
+    },
+  ];
   return (
-    <div className="Vehicles" />
+    <div className="vehicles-list-cont">
+      <div>
+        <h1>List of Vehicles</h1>
+      </div>
+      <div className="list-wrapper">
+        <ul className="list-con">
+          {vehicles.map((vehicle) => (
+            <li className="list-group-item" key={vehicle.id}>
+              <Link to={`/vehicles/${vehicle.id}/details`}>
+                <div className="vehi-card">
+                  <img className="card-img-top" src={vehicle.image} alt="Card cap" />
+                  <div className="card-body">
+                    <h5 className="card-title">{vehicle.name}</h5>
+                    <p className="card-text">{vehicle.price}</p>
+                  </div>
+                </div>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }
 
