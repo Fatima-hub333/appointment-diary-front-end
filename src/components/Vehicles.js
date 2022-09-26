740a22964a43d727b5a3b08555eecf6e6b509e1e
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-26 14:53:40 +0500

    Add vehicle page data

--- src/components/Vehicles.js
+++ src/components/Vehicles.js
@@ -1,6 +1,59 @@
+import { Link } from 'react-router-dom';
+
 function Vehicles() {
+  const vehicles = [
+    {
+      id: 1,
+      name: 'vehicle 1',
+      description: 'This is a description for vehicle 1',
+      image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+      price: '$100',
+    },
+    {
+      id: 2,
+      name: 'vehicle 2',
+      description: 'This is a description for vehicle 2',
+      image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+      price: '$200',
+    },
+    {
+      id: 3,
+      name: 'vehicle 3',
+      description: 'This is a description for vehicle 3',
+      image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+      price: '$300',
+    },
+    {
+      id: 4,
+      name: 'vehicle 4',
+      description: 'This is a description for vehicle 4',
+      image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+      price: '$400',
+    },
+  ];
   return (
-    <div className="Vehicles" />
+    <div className="vehicles-list-cont">
+      <div>
+        <h1>List of Vehicles</h1>
+      </div>
+      <div className="list-wrapper">
+        <ul className="list-con">
+          {vehicles.map((vehicle) => (
+            <li className="list-group-item" key={vehicle.id}>
+              <Link to={`/vehicles/${vehicle.id}`}>
+                <div className="vehi-card">
+                  <img className="card-img-top" src={vehicle.image} alt="Card cap" />
+                  <div className="card-body">
+                    <h5 className="card-title">{vehicle.name}</h5>
+                    <p className="card-text">{vehicle.price}</p>
+                  </div>
+                </div>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }
 
