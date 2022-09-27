78da67579ce6e9a40fbbe37b1bdde54cbe7346ed
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-27 10:48:34 +0500

    Change the deletevehicle view to table

--- src/components/DeleteVehicle.js
+++ src/components/DeleteVehicle.js
@@ -1,72 +1,92 @@
-/* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 
 function DeleteVehicle() {
-  const [vehicles] = useState([
+  const [vehicles, setvehicles] = useState([
     {
       id: 1,
       name: 'vehicle 1',
       description: 'This is a description for vehicle 1',
-      image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
       price: '$100',
+      visible: true,
     },
     {
       id: 2,
       name: 'vehicle 2',
       description: 'This is a description for vehicle 2',
-      image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
       price: '$200',
+      visible: true,
     },
     {
       id: 3,
       name: 'vehicle 3',
       description: 'This is a description for vehicle 3',
-      image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
       price: '$300',
+      visible: true,
     },
     {
       id: 4,
       name: 'vehicle 4',
       description: 'This is a description for vehicle 4',
-      image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
       price: '$400',
+      visible: true,
     },
   ]);
 
-  const handleDelete = (e) => {
+  const handleDelete = (id) => {
+    const newVehicle = vehicles.map(
+      (vehicle) => {
+        const temVehicle = vehicle;
+        if (vehicle.id === id) { temVehicle.visible = false; }
+        return temVehicle;
+      },
+    );
+    setvehicles(newVehicle);
   };
 
   return (
     <div>
-      <div className="text-center">
+      <div className="text-center p-3 header-det">
         <h1>Vehicle List</h1>
       </div>
-      <div className="list-wrapper">
-        <ul className="list-con">
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Name</th>
+            <th scope="col">Price</th>
+            <th scope="col">Delete</th>
+          </tr>
+        </thead>
+        <tbody>
           {vehicles.map((vehicle) => (
-            <li className="list-group-item" key={vehicle.id}>
-              <div className="vehi-card">
-                <img
-                  className="card-img-top"
-                  src={vehicle.image}
-                  alt="Card cap"
-                />
-                <div className="card-body">
-                  <h5 className="card-title">{vehicle.name}</h5>
-                  <p className="card-text">{vehicle.price}</p>
-                </div>
-                <button
-                  type="submit"
-                  className="btn btn-primary add-btn mx-auto"
-                  onClick={() => handleDelete(vehicle.id)}
-                >
-                  Delete
-                </button>
-              </div>
-            </li>
+            <tr key={vehicle.id}>
+              <td>
+                {vehicle.id}
+
+              </td>
+              <td>
+                {vehicle.name}
+
+              </td>
+              <td>
+                {vehicle.price}
+
+              </td>
+              <td>
+                { vehicle.visible ? (
+                  <button
+                    type="submit"
+                    className="btn btn-primary add-btn"
+                    onClick={() => handleDelete(vehicle.id)}
+                  >
+                    Delete
+                  </button>
+                ) : 'This vehicle has been marked' }
+              </td>
+            </tr>
           ))}
-        </ul>
-      </div>
+        </tbody>
+      </table>
     </div>
   );
 }
