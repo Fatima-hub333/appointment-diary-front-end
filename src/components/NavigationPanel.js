4eca622fb673cbf98af1389e37556e953e1032ee
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-27 21:12:59 +0500

    Add open-close menu transitions

--- src/components/NavigationPanel.js
+++ src/components/NavigationPanel.js
@@ -1,40 +1,62 @@
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router';
+import { NavLink } from 'react-router-dom';
+import { Nav } from 'react-bootstrap';
+import { MdMenu, MdFacebook } from 'react-icons/md';
+import {
+  BsTwitter, BsGoogle, BsPinterest, BsInstagram,
+} from 'react-icons/bs';
+import '../styles/NavigationPanel.scss';
 
 function NavigationPanel() {
+  const location = useLocation();
+  const [isMenuOpen, setIsMenuOpen] = useState(true);
+  useEffect(() => {
+    if (['/', '/sign_up'].includes(location.pathname)) setIsMenuOpen(false);
+  }, [location]);
   return (
     <div className="NavigationPanel">
-      <div className="App">
-        <nav>
-          <div>
+      <div className={`nav-container ${isMenuOpen ? 'open' : ''}`}>
+        <MdMenu className="nav-menu-icon" onClick={() => setIsMenuOpen(!isMenuOpen)} />
+        <div className="nav-collalpse">
+          <NavLink className="nav-brand" to="/main">Book.it</NavLink>
+          <Nav>
             <ul>
-              <li className="nav-item">
-                <Link className="nav-link" to="/main">
-                  Vehicles
-                </Link>
+              <li>
+                <NavLink to="/main">Vehicles</NavLink>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/reservation">
-                  Reserve
-                </Link>
+              <li>
+                <NavLink to="/reservation">Reserve</NavLink>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/user/reservations">
-                  My reservations
-                </Link>
+              <li>
+                <NavLink to="/user/reservations">My reservations</NavLink>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/vehicles/new">
-                  Add a vehicle
-                </Link>
+              <li>
+                <NavLink to="/vehicles/new">Add vehicle</NavLink>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/vehicle">
-                  Delete vehicle
-                </Link>
+              <li>
+                <NavLink to="/vehicle">Delete vehicle</NavLink>
               </li>
             </ul>
-          </div>
-        </nav>
+          </Nav>
+        </div>
+        <div className="nav-social-links">
+          <a href="#/">
+            <BsTwitter />
+          </a>
+          <a href="#/">
+            <MdFacebook />
+          </a>
+          <a href="#/">
+            <BsGoogle />
+          </a>
+          <a href="#/">
+            <BsInstagram />
+          </a>
+          <a href="#/">
+            <BsPinterest />
+          </a>
+        </div>
       </div>
     </div>
   );
