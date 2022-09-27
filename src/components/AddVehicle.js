087701bf6882c1b338bf011e436bdcc712754a01
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-27 19:55:06 +0500

    Add basic AddVehicle jsx structure

--- src/components/AddVehicle.js
+++ src/components/AddVehicle.js
@@ -1,38 +1,10 @@
-import { useState } from 'react';
 import {
   Container, Row, Col, Alert, Form, Button,
 } from 'react-bootstrap';
 
 function AddVehicle() {
-  const [errors, setErrors] = useState([]);
-  const [{
-    id, price, name, image,
-  }, setVehicle] = useState({});
+  const errors = ['error 1', 'error 2'];
 
-  const onSelectFile = (e) => {
-    if (!e.target.files || e.target.files.length === 0) {
-      setVehicle({
-        id, name, price, image: undefined,
-      });
-      return;
-    }
-    const file = e.target.files[0];
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => {
-      setVehicle({
-        id, name, price, image: reader.result,
-      });
-    };
-    reader.onerror = () => {
-      setErrors([`Error occurred reading file: ${file.name}`]);
-    };
-  };
-  const handleChange = (e) => {
-    setVehicle({
-      id, name, price, image, [e.target.name]: e.target.value,
-    });
-  };
   return (
     <Form>
       <Container className="AddVehicle">
@@ -48,16 +20,15 @@ function AddVehicle() {
         <Row>
           <Col lg={8}>
             <div className="image-panel">
-              <input type="file" onChange={onSelectFile} />
-              {image ? <img src={image} alt={name} /> : 'Plese select an image' }
+              <input type="file" />
             </div>
           </Col>
           <Col lg={4}>
             <Row>
-              <Form.Control type="text" placeholder="Name" name="name" value={name} onChange={handleChange} />
+              <Form.Control type="text" placeholder="Name" name="name" />
             </Row>
             <Row>
-              <Form.Control type="number" placeholder="Price" name="price" value={price} onChange={handleChange} />
+              <Form.Control type="number" placeholder="Price" name="price" />
             </Row>
             <Row>
               <Button variant="primary" type="submit">Create</Button>
