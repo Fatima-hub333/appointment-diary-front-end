f54665b1fb53f400e7c6ff7bf957e2cf4c66fa88
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-27 10:42:42 +0500

    user can see a list of all items with name and Delete button #31

--- src/components/DeleteVehicle.js
+++ src/components/DeleteVehicle.js
@@ -1,6 +1,73 @@
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
+
 function DeleteVehicle() {
+  const [vehicles] = useState([
+    {
+      id: 1,
+      name: 'vehicle 1',
+      description: 'This is a description for vehicle 1',
+      image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+      price: '$100',
+    },
+    {
+      id: 2,
+      name: 'vehicle 2',
+      description: 'This is a description for vehicle 2',
+      image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+      price: '$200',
+    },
+    {
+      id: 3,
+      name: 'vehicle 3',
+      description: 'This is a description for vehicle 3',
+      image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+      price: '$300',
+    },
+    {
+      id: 4,
+      name: 'vehicle 4',
+      description: 'This is a description for vehicle 4',
+      image: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+      price: '$400',
+    },
+  ]);
+
+  const handleDelete = (e) => {
+  };
+
   return (
-    <div className="DeleteVehicle" />
+    <div>
+      <div className="text-center">
+        <h1>Vehicle List</h1>
+      </div>
+      <div className="list-wrapper">
+        <ul className="list-con">
+          {vehicles.map((vehicle) => (
+            <li className="list-group-item" key={vehicle.id}>
+              <div className="vehi-card">
+                <img
+                  className="card-img-top"
+                  src={vehicle.image}
+                  alt="Card cap"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{vehicle.name}</h5>
+                  <p className="card-text">{vehicle.price}</p>
+                </div>
+                <button
+                  type="submit"
+                  className="btn btn-primary add-btn mx-auto"
+                  onClick={() => handleDelete(vehicle.id)}
+                >
+                  Delete
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }
 
