b2d7d9984d3c77ff0498e085191b969c4af84899
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-27 19:56:11 +0500

    Bind state in AddVehicle

--- src/components/AddVehicle.js
+++ src/components/AddVehicle.js
@@ -1,10 +1,15 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+import { useState } from 'react';
 import {
   Container, Row, Col, Alert, Form, Button,
 } from 'react-bootstrap';
 
 function AddVehicle() {
   const errors = ['error 1', 'error 2'];
-
+  const [{
+    id, price, name, image,
+  }, setVehicle] = useState({});
   return (
     <Form>
       <Container className="AddVehicle">
@@ -21,14 +26,15 @@ function AddVehicle() {
           <Col lg={8}>
             <div className="image-panel">
               <input type="file" />
+              {image ? <img src={image} alt={name} /> : 'Plese select an image' }
             </div>
           </Col>
           <Col lg={4}>
             <Row>
-              <Form.Control type="text" placeholder="Name" name="name" />
+              <Form.Control type="text" placeholder="Name" name="name" value={name} onChange={handleChange} />
             </Row>
             <Row>
-              <Form.Control type="number" placeholder="Price" name="price" />
+              <Form.Control type="number" placeholder="Price" name="price" value={price} onChange={handleChange} />
             </Row>
             <Row>
               <Button variant="primary" type="submit">Create</Button>
