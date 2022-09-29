965c0e7be8b42e3ae33c9bcbf05072161620f186
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-29 23:34:09 +0500

    setup mock functions for login and logout

--- src/redux/user/user.js
+++ src/redux/user/user.js
@@ -1,6 +1,39 @@
-export default function reducer(state = { user: { id: 1, name: 'Hayya' }, errors: [], notices: [] }, action = {}) {
-  switch (action.type) {
-    default:
-      return state;
-  }
-}
+/* eslint-disable no-unused-vars */
+const LOGIN_REQUEST = 'LOGIN_REQUEST';
+const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+const LOGIN_FAILURE = 'LOGIN_FAILURE';
+const LOGOUT_REQUEST = 'LOGOUT_REQUEST';
+const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
+const LOGOUT_ERROR = 'LOGOUT_ERROR';
+
+const initialUser = {
+  id: null,
+  username: null,
+  email: null,
+};
+
+const defaultState = {
+  loading: false,
+  error: null,
+  user: initialUser,
+};
+
+const MockLogin = (user) => new Promise((resolve, reject) => {
+  setTimeout(() => {
+    if (user.email === 'user@gmail.com' && user.password === 'password') {
+      resolve({
+        id: 1,
+        username: 'test_user',
+        email: 'user@gmail.com',
+      });
+    } else {
+      reject(new Error('Invalid email or password'));
+    }
+  }, 3000);
+});
+
+const MockLogout = () => new Promise((resolve) => {
+  setTimeout(() => {
+    resolve();
+  }, 3000);
+});
