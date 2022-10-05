a05ec850a0444a1cbffb5bbcebeb474d813ef561
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-10-05 18:48:02 +0500

    Changed hashrouter with browser router

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import store from './redux/configureStore';
-// import './index.css';
+import './index.sass';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
 );
