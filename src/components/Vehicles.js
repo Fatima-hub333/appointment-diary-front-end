cffb3875ca16c607a75dd5d6e0fdda5872610f43
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-10-06 21:00:18 +0500

    Update styling

--- src/components/Vehicles.js
+++ src/components/Vehicles.js
@@ -1,41 +1,66 @@
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import {
+  TiSocialTwitterCircular,
+  TiSocialInstagramCircular,
+  TiSocialFacebookCircular,
+} from 'react-icons/ti';
+import Carousel from 'react-bootstrap/Carousel';
+import { loadVehicles } from '../redux/vehicles/vehicles';
 import '../styles/Vehicles.scss';
 
-const Vehicles = () => {
-  const vehicles = useSelector((state) => state.vehicles.all);
+function Vehicles() {
+  const dispatch = useDispatch();
+  const vehicles = useSelector((state) => state.vehicles.visible);
+  useEffect(() => {
+    dispatch(loadVehicles());
+  }, []);
   return (
-    <div className="vehicles-list-cont">
-      <div>
-        <h1>List of Vehicles</h1>
-      </div>
-      <div className="list-wrapper">
-        <ul className="list-con">
-          {vehicles.map((vehicle) => (
-            <li className="list-group-item" key={vehicle.id}>
-              <Link to={`/vehicles/${vehicle.id}/details`}>
-                <div className="vehi-card">
-                  <img
-                    className="card-img-top"
-                    src={vehicle.image}
-                    alt="Card cap"
-                  />
-                  <div className="card-body">
-                    <h5 className="card-title">
-                      {vehicle.brand}
-                      {' '}
-                      {vehicle.model}
-                    </h5>
-                    <p className="card-text">{vehicle.price}</p>
+    <Carousel>
+      <Carousel.Item className="vehicles-list-cont">
+        <div className="text-center header-det">
+          <h1>List of Vehicles</h1>
+        </div>
+        <div className="text-center subTitle">
+          <h6>Our vintage machine</h6>
+        </div>
+        <div className="list-wrapper">
+          <ul className="d-flex list-con">
+            {vehicles.map((vehicle) => (
+              <li className="list-group-item" key={vehicle.id}>
+                <Link to={`/vehicles/${vehicle.id}/details`} className="vehiLink-button">
+                  <div className="vehi-card">
+                    <img className="viewCard-img-top" src={vehicle.image} alt="Card cap" />
+                    <div className="viewCard-body">
+                      <h5 className="viewCard-title">{vehicle.name}</h5>
+                      <p className="viewCard-text">
+                        $
+                        {vehicle.price}
+                      </p>
+                    </div>
+                    <div>
+                      <ul className="item-icons d-flex">
+                        <li className="socialImg">
+                          <TiSocialFacebookCircular />
+                        </li>
+                        <li className="socialImg">
+                          <TiSocialTwitterCircular />
+                        </li>
+                        <li className="socialImg">
+                          <TiSocialInstagramCircular />
+                        </li>
+                      </ul>
+                    </div>
                   </div>
-                </div>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </Carousel.Item>
+    </Carousel>
   );
-};
+}
 
 export default Vehicles;
